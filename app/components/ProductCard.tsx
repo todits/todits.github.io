b2e14f989eb68493746f3aff28937a8aea5a25e0
todits <onlayao111@gmail.com>
2024-08-08 19:51:42 +0800

    check

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import AddToCart from "./AddToCart";
+import styles from "./ProductCard.module.css";
+
+const ProductCard = () => {
+   return (
+      <div className={styles.card}>
+         <AddToCart />
+      </div>
+   );
+};
+
+export default ProductCard;
